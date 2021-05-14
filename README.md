5bb703e83d1eb361780e39a8640c1ff7d414eadd
victorlikhachov <antos2512@hotmail.com>
2021-05-14 13:19:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-yxryuy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-yxryuy)
---
+++ patch-reducer.ts
@@ -0,0 +1,17 @@
+import { Dispatch, useReducer } from "react";
+
+function usePatchReducer<TState,TAction>(patchfn: (state: TState,action: TAction) => Partial<TState>,
+                                         initial:TState) : [TState,Dispatch<TAction>];
+function usePatchReducer<TState,TAction>(patchfn: (state: TState,action: TAction) => Partial<TState>,
+                                         initial:TState,
+                                         initializer: (state: TState)=> TState ) : [TState,Dispatch<TAction>];
+function usePatchReducer<TState,TAction>(patchfn: (state: TState,action: TAction) => Partial<TState>,
+                                         initial:TState,
+                                         initializer?: (state: TState)=> TState ) : [TState,Dispatch<TAction>] {
+    const applyPatchReducer=<TState,TAction>(source:TState,action: TAction, patchfn: (state: TState,action: TAction) => Partial<TState>) : TState => {
+        const patch = patchfn(source,action)
+        return {...source,...patch};
+    }
+    const fn = (initializer) ? initializer : (arg:TState) => {return arg;};
+    return useReducer((s:TState ,a:TAction) => applyPatchReducer(s,a,patchfn),initial,fn);
+}
