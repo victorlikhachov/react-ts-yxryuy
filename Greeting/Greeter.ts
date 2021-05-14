843708d0cf5ed9b13f8fb8d0338abd480dc58ba4
victorlikhachov <antos2512@hotmail.com>
2021-05-14 13:34:38 -0400

    3 files added

---
+++ Greeting/Greeter.ts
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Greeter: React.FC<IGreetingProps> = ({ greeting }) => {
+  const responseReducer = (
+    _state: IResponse,
+    action: IResponseAction
+  ): Partial<IResponse> => {
+    const { actionType, payload } = action;
+    switch (actionType) {
+      case 'actor':
+        return { actor: payload };
+      case 'response':
+        return { response: payload };
+    }
+  };
+
+  const init = (init: IResponse): IResponse => {
+    return init;
+  };
+
+  const [response, responseDispatch] = usePatchReducer(
+    responseReducer,
+    { response: '', actor: '' },
+    init
+  );
+  return (
+    <>
+      <h1 style={{ color: 'blue' }}>{greeting.message}</h1>
+      <input
+        type="text"
+        value={response.response}
+        onChange={e =>
+          responseDispatch({ actionType: 'response', payload: e.target.value })
+        }
+      />
+    </>
+  );
+};
---
+++ models/IGreeting.ts
@@ -0,0 +1,4 @@
+export interface IGreeting {
+  message: string;
+  sender: string;
+}
